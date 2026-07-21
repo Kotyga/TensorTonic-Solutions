61e42e60cde885619d1d80f6d12ed0c223e5078e
Maiia Kotyga <74975853+Kotyga@users.noreply.github.com>
2026-07-21 22:46:25 +0300

    Solve mean-median-mode

---
+++ mean-median-mode/mean-median-mode.py
@@ -0,0 +1,10 @@
+import numpy as np
+from collections import Counter
+
+def mean_median_mode(x):
+    x = np.asarray(x)
+    mean = np.mean(x)
+    median = np.median(x)
+    counts = Counter(x)
+    mode = counts.most_common(1)[0][0]
+    return mean, median, mode
